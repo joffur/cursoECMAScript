ed74a8d5bb2131a791413ddf62a68b8d2d4ca94e
Jose Fernando Fuentes Roa <ing.josefuentes@hotmail.com>
2023-06-28 06:31:06 -0500

    Clase - ES6: spread operator

--- src/ES6/04-rest-spread.js
+++ src/ES6/04-rest-spread.js
@@ -9,3 +9,21 @@ console.log({a, b});
 let user = { username: 'Jose', age: 34 };
 let { username, age } = user;
 console.log({username, age});
+
+// Spread operator
+
+let person = { name: 'Jose', age: 28 };
+let country = 'CO';
+
+let data = { id:1, ...person, country };
+console.log(data);
+
+// rest
+
+function sum(num, ...values) {
+  console.log(values);
+  console.log(num + values[0]);
+  return num + values[0];
+}
+
+sum(1, 1, 2, 3);
