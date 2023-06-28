bd53ca52b0982455e8eeabdb9383e947db7a4b61
Jose Fernando Fuentes Roa <ing.josefuentes@hotmail.com>
2023-06-28 06:12:56 -0500

    Clase - ES6: asignación de desestructuración

---
+++ src/ES6/04-rest-spread.js
@@ -0,0 +1,11 @@
+// Arrays destructuring
+
+let fruits = ['Apple', 'Banana'];
+let [a, b] = fruits;
+console.log({a, b});
+
+// Object destructuring
+
+let user = { username: 'Jose', age: 34 };
+let { username, age } = user;
+console.log({username, age});
