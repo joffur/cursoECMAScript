21b0a38d07e7f5984cd5dfedbbe8a402ea3e2b02
Jose Fernando Fuentes Roa <ing.josefuentes@hotmail.com>
2023-06-29 07:01:29 -0500

    Clase - ES11: optional chaining

---
+++ src/ES11/00-optional-chaining.js
@@ -0,0 +1,11 @@
+const users = {
+  joffur: {
+    country: 'CO'
+  },
+  ana: {
+    country: 'MX'
+  },
+}
+
+console.log(users.joffur);
+console.log(users?.gndx?.country);
---
+++ src/ES11/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+const users = {
+  vf: {
+      country : 'MX'
+  },
+  fr : {
+      country: 'CO'
+  }
+}
+
+
+console.log(users.vf?.country);
+console.log(users?.bd?.country);
